6ee642c80b16bf0333c22a55ac3ff21ebe273a08
YauryRP <149832373+YauryRP@users.noreply.github.com>
2026-09-07 14:45:10 +0200

    Add files via upload

---
+++ ultrasonicRanging/ultrasonicRanging.ino
@@ -0,0 +1,32 @@
+#define trigPin 13 
+#define echoPin 14 
+#define MAX_DISTANCE 700 
+
+float timeOut = MAX_DISTANCE * 60;
+int soundVelocity = 340; 
+void setup() {
+pinMode(trigPin,OUTPUT);
+pinMode(echoPin,INPUT); 
+Serial.begin(115200);   
+}
+
+void loop() {
+delay(100); 
+Serial.printf("Distance: ");
+Serial.print(getSonar()); 
+Serial.println("cm");
+}
+
+float getSonar() {
+unsigned long pingTime;
+float distance;
+
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+pingTime = pulseIn(echoPin, HIGH, timeOut);
+
+distance = (float)pingTime * soundVelocity / 2 / 10000;
+return distance; 
+}
